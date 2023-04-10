cb96869991e6c41df7bf7a045670357c0d6db0f2
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-04-11 02:10:14 +0400

    Update RUN.py

--- RUN.py
+++ RUN.py
@@ -1,10 +1,12 @@
 import os,time,platform
 os.system('clear')
-print('[>] Checking Updates')
+#print('[>] Checking Updates')
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
     import SHARE
+elif bit=='32bit':
+    import SH32
 else:
-    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
+    print('\033[1;31m[×] unknown Device 32')
  
